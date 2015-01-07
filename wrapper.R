09cae3ab1a935ea0d6c0943f7a22c6da9c73ac08
root <root@ip-10-186-17-176.ec2.internal>
2015-01-07 13:17:13 -0500

    awesome wrapper - Rscript ./hackathon_v001_metagenomics/wrapper.R db=refseq_viral/viral.1.1.genomic.fna HMP/illumina/right_retroauricular_crease/SRS*

--- wrapper.R
+++ wrapper.R
@@ -1,28 +1,29 @@
 #!/usr/bin/env Rscript
-download_not_installed<-function(x){
-	for(i in x){
-		if(!require(i,character.only=TRUE)){
-	  		install.packages(i,repos="http://cran.r-project.org")
-  			library(i,character.only=TRUE)
-  		}
-	}
-}
-download_not_installed(doParallel)
 rm(list=ls())
 args <- commandArgs(TRUE)
 if(length(args)<3){
   stop("Usage: wrapper.R db=''viral_refseq.fna'' directory1 directory2 ...")
 }
 bin = "hackathon_v001_metagenomics"
-cores = 12
+cores = 15
 viralDb = eval(parse(text=args[1]))
 
-cl = makeCluster(cores)
-registerDoParallel(cl)
+download_not_installed<-function(x){
+        for(i in x){
+                if(!require(i,character.only=TRUE)){
+                        install.packages(i,repos="http://cran.r-project.org")
+                        library(i,character.only=TRUE)
+                }
+        }
+}
+download_not_installed("doParallel")
 
 # Run sra-blastn
 dirs = args[2:length(args)]
 resultNames = dirs
+
+cl = makeCluster(cores)
+registerDoParallel(cl)
 
 foreach(i=1:length(dirs)) %dopar% {
 	print(sprintf("Running file %s",dirs[i]))
@@ -30,12 +31,17 @@ foreach(i=1:length(dirs)) %dopar% {
 	cmd = sprintf("Rscript %s/blastn_sample.R db=\"'%s'\" %s",bin,viralDb,dirFiles)
 	system(cmd)
 }
-# check resultNames is not a list
-#print("Summarizing samples")
-#sraBlastCmd = sprintf("Rscript %s/summarize_samples.R o=\"'%s'\" %s",bin,outputFile,resultNames)
-#print(sraBlastCmd)
-#system(sraBlastCmd)
 
+stopCluster(cl)
+
+print("Summarizing samples into results.tsv")
+blastFiles = paste(paste(resultNames,"*blast.results",sep="/"),collapse=" ")
+sraBlastCmd = sprintf("Rscript %s/summarize_samples.R %s",bin,blastFiles)
+print(sraBlastCmd)
+system(sraBlastCmd)
+
+print("regular blastn")
 # Run regular blastn
 
+print("contig blastn")
 # Run contig blastn
