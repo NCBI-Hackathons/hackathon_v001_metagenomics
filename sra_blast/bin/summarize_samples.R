13d149ef2937113a1f046e4693ce520941aaa235
Joseph N. Paulson <jpaulson@umiacs.umd.edu>
2015-01-12 15:13:56 -0500

    Update summarize_samples.R

--- sra_blast/bin/summarize_samples.R
+++ sra_blast/bin/summarize_samples.R
@@ -2,18 +2,22 @@
 rm(list=ls())
 args <- commandArgs(TRUE)
 
-# Assumes output from 'blastn_sample.R'
-# Really it's just blast outfmt = 6 and first column = qseqid.
+# Input:
+# 	Blast results for any number of samples
+# Assumes output from 'sra_blast.sh' or 'blastn_vdb_wrapper.sh' 
+# Input requires the first column represents the row name (query sequence id) of a sample.
+# In particular, the output of the blast results are outfmt = 6 and first column = qseqid.
 #
-# Assumes (which it shouldn't pragmatically) first argument is
-# 	the name for outfile and specifically called as o='path/filename' 
-#   without spaces.
+# Output:
+# 	results.tsv - tab delimited file where rows are features, columns are samples
+#			and each cell represents the number of hits for a given feature/sample.
+#
 if(length(args)<1){
-  stop("Usage: summarize_samples.R file1 file2 ...")
+  stop("Usage: Rscript summarize_samples.R file1 file2 ...")
 }
 fileNames = basename(args)
 fileResults = lapply(args,function(i){
-		tryCatch(read.csv(i,header=FALSE,stringsAsFactors=FALSE,"\t"),error=function(e){})
+		tryCatch(read.csv(i,header=FALSE,stringsAsFactors=FALSE,"\t"),error=function(e){print(e)})
 	})
 virusesFound = lapply(fileResults,function(i){
 		table(i[,1])
@@ -27,7 +31,6 @@ vlist = unique(vlist)
 countMatrix = array(0,dim=c(length(vlist),length(fileNames)))
 colnames(countMatrix) = fileNames
 rownames(countMatrix) = vlist
-
 for(i in fileNames){
 	countMatrix[names(virusesFound[[i]]),i] = virusesFound[[i]]
 }
