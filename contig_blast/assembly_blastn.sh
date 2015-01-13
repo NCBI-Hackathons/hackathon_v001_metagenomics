76b6bea8a28a9409665f0010c7cfb56126731339
nosson <nosson@gmail.com>
2015-01-13 11:47:36 -0500

    fixing contig blast script

--- contig_blast/assembly_blastn.sh
+++ contig_blast/assembly_blastn.sh
@@ -1,60 +1,20 @@
 #!/usr/bin/sh
-# Argument = -t test -r server -p password -v
 
 # Initialize timestamp variables
 DATE=`date +%Y-%m-%d`
 TIME=`date +%H:%M`
 TM=`date +%Y%m%d-%H%M`
-LOG=SRA-Blast_virome_log_$TM.txt
+LOG=Blast_virome_log_$TM.txt
 
-usage()
-{
-cat << EOF
-usage: $0 options
+READS1=$1
+READS2=$2
+DB=$3
+#Create log file
+echo "Executing Assembly using Abyss on $DATE at $TIME " | tee $LOG
+abyss-pe name=assembly k=25 in='$READS1 $READS2' np=30 | tee -a $LOG
 
-This script run the test1 or test2 over a machine.
+echo "Executing make blast DB on $DATE at $TIME " | tee -a $LOG
+makeblastdb -in $DB -dbtype 'nucl' -out ./DB_out
 
-OPTIONS:
-   -h      Show this message
-   -t      Test type, can be ‘test1′ or ‘test2′
-   -r      Server address
-   -p      Server root password
-   -v      Verbose
-EOF
-}
-
-TEST=
-SERVER=
-PASSWD=
-VERBOSE=
-while getopts “ht:r:p:v” OPTION
-do
-     case $OPTION in
-         h)
-             usage
-             exit 1
-             ;;
-         t)
-             TEST=$OPTARG
-             ;;
-         r)
-             SERVER=$OPTARG
-             ;;
-         p)
-             PASSWD=$OPTARG
-             ;;
-         v)
-             VERBOSE=1
-             ;;
-         ?)
-             usage
-             exit
-             ;;
-     esac
-done
-
-if [[ -z $TEST ]] || [[ -z $SERVER ]] || [[ -z $PASSWD ]]
-then
-     usage
-     exit 1
-fi
+echo "Executing blasting contig on $DATE at $TIME " | tee -a $LOG
+blastn -task blastn -db $DB -query assembly-contigs.fa -out assembly_contigs_vs_retrovirus.blast -outfmt '6 qseqid sseqid slen pident length mismatch gapopen qstart qend sstart send evalue bitscore' -num_threads 30 | tee -a $LOG
